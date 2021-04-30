74b406308691086b9623d534f69f5ad1e5057134
jacob <jacobharv00@gmail.com>
2021-04-30 15:21:31 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+const text = newHeader.textContent = "Jacob Harvey is the champion"
+document.querySelector('#victory').appendChild(text)
+
+
+
+
