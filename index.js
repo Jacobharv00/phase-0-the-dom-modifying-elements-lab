f6b84c612df3215ad9cfe7d18e5564bd98ec9cb3
jacob <jacobharv00@gmail.com>
2021-06-14 18:47:57 -0600

    Done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ main.remove()
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory'
 const text = newHeader.textContent = "Jacob Harvey is the champion"
-document.querySelector('#victory').appendChild(text)
+document.body.append(newHeader)
 
 
 
